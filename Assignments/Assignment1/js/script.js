1162c98942cbb4f86a57c0c076be22b7ae51177d
c-gancz <christina_gancz@hotmail.ca>
2017-02-05 15:40:06 -0500

    Restarted script - new ideas.

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -1,17 +1,5 @@
 $(document).ready(function (){
 
-mySquares();
+
 
 });
-
-function mySquares (){
-
-  var divsToAdd = 100;
-  var currentDiv = 0;
-
-  while (currentDiv < divsToAdd) {
-    var square = $('<div class = square></div>');
-    $('body').append(square);
-    currentDiv = currentDiv + 1;
-  }
-}
