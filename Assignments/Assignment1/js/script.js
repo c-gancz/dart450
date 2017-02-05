2f1c2a89817389b426a1cc5a473947c4f160382c
c-gancz <christina_gancz@hotmail.ca>
2017-02-05 16:55:48 -0500

    Created openDoors() function to open and close doors.

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function (){
 
-
+  openDoors();
 
 });
+
+//////////////////// FUNCTIONS APPEAR HERE /////////////////////
+
+function openDoors (){
+  var leftDoor = $('#left');
+  var rightDoor = $('#right');
+  var bothDoors = $('#twodoors');
+
+$(bothDoors).click(function (){
+  $(leftDoor).css({
+    'margin-left':'-600px',
+    'position:':'absolute'
+  });
+
+  $(rightDoor).css({
+    'margin-left':'1200px',
+    'position':'absolute'
+  });
+
+});
+
+}
