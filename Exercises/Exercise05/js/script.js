5640f60b5244b6b3a9e742c41f28fbd08841b523
c-gancz <c_gancz@cda037ev5709x.local>
2017-02-07 17:22:48 -0500

    Added various voice recognitions.

--- Exercises/Exercise05/js/script.js
+++ Exercises/Exercise05/js/script.js
@@ -4,7 +4,9 @@ $(document).ready(function (){
 
 });
 
+
 function nameResponse (){
+
   $('#name').click(function (){
     var voice = 'US English Female';
     var voiceParameters = {
@@ -12,7 +14,20 @@ function nameResponse (){
       rate: 0.9,
       volume: 1
     }
-
     responsiveVoice.speak("I don't like that name. Change your name!", voice, voiceParameters);
+    responsiveVoice.speak("No seriously, change it.", voice, voiceParameters);
+    responsiveVoice.speak("It's the worst name I've ever heard.", voice, voiceParameters);
+
+  $('#firstname').hide();
+
+  $('#newname').show(function (){
+    $('#name2').click(function(){
+      responsiveVoice.speak("Oh for the love of God, that name is more pathetic than the first one.", voice, voiceParameters);
+      responsiveVoice.speak("I think you should just leave the webpage now, this is just embarassing for all of us.", voice, voiceParameters);
+      responsiveVoice.speak("Goodbye person with the worst name in history.", voice, voiceParameters);
+    });
+  });
+
+
   });
 }
