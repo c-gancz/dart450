d5368ec82c49b03587b297d9be7c5839af859b5d
c-gancz <christina_gancz@hotmail.ca>
2017-02-12 13:56:03 -0500

    Added z-index to css to get layering in elements.

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -1,37 +1,7 @@
 $(document).ready(function (){
 
-  $(openDoors).click(myCircles);
+
 
 });
 
 //////////////////// FUNCTIONS APPEAR HERE /////////////////////
-
-function openDoors (){
-  var leftDoor = $('#left');
-  var rightDoor = $('#right');
-  var bothDoors = $('#twodoors');
-
-$(bothDoors).click(function (){
-  $(leftDoor).css({
-    'margin-left':'-600px',
-    'position:':'absolute'
-  });
-
-  $(rightDoor).css({
-    'margin-left':'1200px',
-    'position':'absolute'
-  });
-
-});
-
-}
-
-function myCircles (){
-  var circle1 = $('<div class="circle"></div>');
-  var circle2 = $('<div class="circle"></div>');
-  var circle3 = $('<div class="circle"></div>');
-  var circle4 = $('<div class="circle"></div>');
-  var circles = [circle1, circle2, circle3, circle4]
-
-  $('body').append(circles);
-}
