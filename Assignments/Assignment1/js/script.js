da325b2f86c29cbfa468d0c776a8a59bc9e54a77
c-gancz <christina_gancz@hotmail.ca>
2017-02-12 23:40:12 -0500

    Added fading function to input + doors to get to main page.

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -33,12 +33,17 @@ function inputValue (){
 
   $('button').click(function(){
     var inputValue = $('#inputvalue').val();
-    var voiceResponse = 'Hey there' + inputValue + 'You have an interesting name. Not as interesting as mine though, and definetly not as interesting as my life. Sit back and take a few notes on how my mind functions on a daily basis.';
+    var voiceResponse = 'Hey there' + inputValue + 'You have an interesting name. Not as interesting as mine though, and definetly not as interesting as my life.';
+    var inputDisappear = $('.input');
+    var doorsDisappear = $('.sliding-doors');
 
     responsiveVoice.speak(voiceResponse, "US English Female", {
       rate:0.7,
       pitch:1
     });
+
+    $(inputDisappear).remove();
+    $(doorsDisappear).fadeOut(2000);
   });
 
 };
