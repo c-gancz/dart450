161f5f677519151c4325af5759055b56f9f6d0f7
c-gancz <christina_gancz@hotmail.ca>
2017-02-14 22:15:49 -0500

    Added black background change to responsive voice.

--- Exercises/Exercise05/js/script.js
+++ Exercises/Exercise05/js/script.js
@@ -14,17 +14,17 @@ function nameResponse (){
       rate: 0.9,
       volume: 1
     }
-    responsiveVoice.speak("I don't like that name. Change your name!", voice, voiceParameters);
-    responsiveVoice.speak("No seriously, change it.", voice, voiceParameters);
-    responsiveVoice.speak("It's the worst name I've ever heard.", voice, voiceParameters);
+    responsiveVoice.speak("I don't like that name. Change your name... No seriously, change it.", voice, voiceParameters);
 
   $('#firstname').hide();
 
   $('#newname').show(function (){
     $('#name2').click(function(){
-      responsiveVoice.speak("Oh for the love of God, that name is more pathetic than the first one.", voice, voiceParameters);
-      responsiveVoice.speak("I think you should just leave the webpage now, this is just embarassing for all of us.", voice, voiceParameters);
-      responsiveVoice.speak("Goodbye person with the worst name in history.", voice, voiceParameters);
+      responsiveVoice.speak("Oh for the love of God, that name is more pathetic than the first one. I think you should just leave the webpage now, this is just embarassing for all of us.", voice, voiceParameters);
+      $('section').remove();
+      $('body').css({
+        'background-color':'black'
+      });
     });
   });
 
