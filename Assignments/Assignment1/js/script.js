8cafa9226237bbcbe3fd7348d3ac4ffea486ddb5
c-gancz <christina_gancz@hotmail.ca>
2017-02-15 20:29:07 -0500

    Added an automatic redirect to another page on click, depending on the day.

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -7,7 +7,7 @@ introDoors();
 inputResponse();
 
 $(enterButton).click(function(){
-      setInterval(showDay,2000);
+      setInterval(showDay,10000);
 });
 
 });
@@ -47,25 +47,25 @@ function inputResponse (){
     var todayName = 'Nothing.'
 
     if (today == 0) {
-      todayName = 'Sunday';
+      todayName = 'Sunday, which is a rather fun day.';
     }
     else if (today == 1) {
       todayName = 'Monday, which is kind of a crappy day.';
     }
     else if (today == 2) {
-      todayName = 'Tuesday';
+      todayName = 'Tuesday, which is kind of long day.';
     }
     else if (today == 3) {
-      todayName = 'Wednesday';
+      todayName = 'Wednesday, which is an okay day.';
     }
     else if (today == 4) {
-      todayName = 'Thursday';
+      todayName = 'Thursday, which is an exhausting day.';
     }
     else if (today == 5) {
-      todayName = 'Friday';
+      todayName = 'Friday, which sucks because I work.';
     }
     else if (today == 6) {
-      todayName = 'Saturday';
+      todayName = 'Saturday, which is a homework-filled day.';
     }
 
     var voiceResponse = 'Hello there,' + inputValue + ', Welcome to my life on a weekly basis. Today is ' + todayName;
@@ -84,5 +84,36 @@ function inputResponse (){
 };
 
 function showDay(){
-  $('#block').show();
+  var date = new Date();
+  var today = date.getDay();
+  var weeklyPage = ["html/sunday.html", "html/monday.html", "html/tuesday.html", "html/wednesday.html", "html/thursday.html", "html/friday.html", "html/saturday.html"]
+
+  if (today == 0) {
+    location.href = weeklyPage[0];
+  }
+
+  else if (today == 1) {
+    location.href = weeklyPage[1];
+  }
+
+  else if (today == 2) {
+    location.href = weeklyPage[2];
+  }
+
+  else if (today == 3) {
+    location.href = weeklyPage[3];
+  }
+
+  else if (today == 4) {
+    location.href = weeklyPage[4];
+  }
+
+  else if (today == 5) {
+    location.href = weeklyPage[5];
+  }
+
+  else if (today == 6) {
+    location.href = weeklyPage[6];
+  }
+
 };
