9856b021487beb7d8a0744f0526bba41ee777de9
c-gancz <christina_gancz@hotmail.ca>
2017-03-06 16:26:33 -0500

    Added a title fade.

--- Exercises/Exercise07/js/script.js
+++ Exercises/Exercise07/js/script.js
@@ -2,21 +2,56 @@
 
 $(document).ready(function() {
 
-  var controller = new ScrollMagic.Controller();
+  titleFade();
 
-// Defines the trigger and the duration of scrolling to animate it
-  var scene = new ScrollMagic.Scene({
-    triggerElement: "#trigger1",
-    duration: 1000
-  });
+  titleSlide();
 
-  // 1: time before trigger begins animation
-  scene.setTween("#trigger1", 0.3, {
-    'width':'600px',
-  });
-
-  scene.triggerHook(1);
-
-  scene.addTo(controller);
 
 });
+
+// UNIVERSAL VARIABLE, WILL WORK FOR ALL FUNCTIONS
+var controller = new ScrollMagic.Controller();
+
+function titleFade(){
+  var title = new ScrollMagic.Scene({
+    triggerElement: ".header",
+    duration: 200
+  });
+
+  title.setTween(".header", 0.1, {
+    'opacity': '1',
+  });
+
+  title.triggerHook(0.6);
+
+  title.addTo(controller);
+
+  var subtitle = new ScrollMagic.Scene({
+    triggerElement: ".subtitle",
+    duration: 300
+  });
+
+  subtitle.setTween(".subtitle", 0.1, {
+    'marginLeft': '180px',
+    'opacity':'1'
+  });
+
+  subtitle.triggerHook(0.6);
+
+  subtitle.addTo(controller);
+};
+
+function titleSlide(){
+  var background = new ScrollMagic.Scene({
+    triggerElement: "#trigger1",
+    duration: 500
+  });
+
+  background.setTween("#trigger1", 0.1, {
+    'backgroundColor': 'rgba(255, 81, 34, 1)',
+  });
+
+  background.triggerHook(0);
+
+  background.addTo(controller);
+};
