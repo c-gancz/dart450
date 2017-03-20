07a57695d60efc1e7e6871e58dbcd107efd4d0be
c-gancz <christina_gancz@hotmail.ca>
2017-03-20 18:23:37 -0400

    Added a webcam tracking feature that tracks the face specifically.

--- Assignments/Assignment2/js/index.js
+++ Assignments/Assignment2/js/index.js
@@ -17,49 +17,10 @@ window.onload = function() {
           context.strokeStyle = '#a64ceb';
           context.strokeRect(rect.x, rect.y, rect.width, rect.height);
           context.font = '11px Helvetica';
-          context.fillStyle = "#fff";
+          context.fillStyle = "black";
           context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
           context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-          
-//           ------- START - here is your playground -------
-//           WE GET:
-//           x position is: rect.x
-//           y position is: rect.y
-//           face box width: rect.width          
-//           face box height: rect.height
-          
-//           wanna know more read https://trackingjs.com/docs.html
 
-           // lets pass the data to our object somehow
-           $('.object').css({
-             'margin-left' : rect.x, // move the object form left
-             'margin-top' : rect.y, // move the object from top
-             'height': rect.height, // set its height
-             'width' : rect.width // set its width
-            })
-          
-          console.log(rect.x, rect.y) // write some info to the concole
-            
-          // do something if we get some data
-          if(rect.x && rect.y){
-                $('.object').css({
-                  'background' : 'green'
-                })
-              $('.message').text('Got ya')
-          }
-          // do something if we do not get the data
-          else{
-                $('.object').css({
-                  'background' : 'red'
-                })
-                 $('.message').text('Come BACK!')
-           }
-          //  --------- END OF YOUR PLAYGROUND-----------
         });
       });
-
-      var gui = new dat.GUI();
-      gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
-      gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
-      gui.add(tracker, 'stepSize', 1, 5).step(0.1);
     };
