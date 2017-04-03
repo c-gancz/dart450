2979aafcd2fd2d2018804958f2f5ed782b6afe25
c-gancz <c_gancz@cda134ev5635x.local>
2017-04-03 14:28:46 -0400

    Changed some styling properties of the sections

--- Assignments/Assignment2/js/dashboard.js
+++ Assignments/Assignment2/js/dashboard.js
@@ -1,13 +1,20 @@
 
 $(document).ready(function() {
-  // the ui-resizable-handles are added here
-  //http://codepen.io/jamiejefferson/pen/qLAwj
-  $('.resizable').resizable();
 
-  // makes GSAP Draggable avoid clicks on the resize handles
-  $('.ui-resizable-handle').attr('data-clickable', true);
-
-  // make the element draggable
-  Draggable.create('.draggable');
+  dragBox();
 
 });
+
+// FUNCTION 1: Draggable and extendable sections
+// Inspired by Jamie Jefferson's pen on CodePen at: //http://codepen.io/jamiejefferson/pen/qLAwj
+function dragBox(){
+
+  // Makes the sections resizable.
+  $('.resizable').resizable();
+
+  // Avoid clicks on the resize handles.
+  $('.ui-resizable-handle').attr('data-clickable', true);
+
+  // Makes the elements draggable.
+  Draggable.create('.draggable');
+};
