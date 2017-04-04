071011ad1d67b152c655ef8bc0c233ef2bc3b75d
c-gancz <christina_gancz@hotmail.ca>
2017-04-03 21:20:36 -0400

    Added some buttons to enable and disable my functions

--- Assignments/Assignment2/js/dashboard.js
+++ Assignments/Assignment2/js/dashboard.js
@@ -1,20 +1,26 @@
 
 $(document).ready(function() {
 
-  dragBox();
+
+  $('#enlarge-on').click(enlarge);
+
+  $('#rearrange-on').click(rearrange);
+
 
 });
 
 // FUNCTION 1: Draggable and extendable sections
 // Inspired by Jamie Jefferson's pen on CodePen at: //http://codepen.io/jamiejefferson/pen/qLAwj
-function dragBox(){
-
+function enlarge(){
   // Makes the sections resizable.
   $('.resizable').resizable();
 
   // Avoid clicks on the resize handles.
   $('.ui-resizable-handle').attr('data-clickable', true);
+};
 
+function rearrange(){
   // Makes the elements draggable.
   Draggable.create('.draggable');
+
 };
