aa35fdaf4871d0f3cebed12d5faa88ebfba08a67
c-gancz <christina_gancz@hotmail.ca>
2017-04-29 14:35:12 -0400

    Added some commenting

--- Assignments/Assignment2/js/psychiatrist_counsellor.js
+++ Assignments/Assignment2/js/psychiatrist_counsellor.js
@@ -19,21 +19,63 @@ $(document).ready(function(){
               // If statement allowing the Eliza Robot to only appear when the width of the psychologist section
               // is larger than 500px and height longer than 600px.
               if (widthPsychologist > 500 && heightPsychologist > 600) {
-                $('#elizarobot').show();
+                elizaReveal();
               }
 
               // Else statement hiding the Eliza Robot when width of the psychologist section is smaller
               // than 500px and height smaller than 600px.
               else {
-                $('#elizarobot').hide();
+                elizaConceal();
               };
-
 
           } // Closing the jQuery UI resize function.
     }); // Closing the entire psychologist reveal/conceal function.
 
 }); // Closing the function of the enlarge-on button.
 
-
-
 }); //End of document.ready function
+
+/////////////////////////  FUNCTIONS  //////////////////////////////
+
+// FUNCTION 1: What will be revealed when the psychologist section is enlarged.
+function elizaReveal(){
+
+  // Show the eliza robot
+  $('#elizarobot').show();
+
+  // Make the psychologist background green
+  $('#psychologist').css({
+    'background-color':'green',
+  });
+
+  // Make the image get larger
+  $('#psychologist img').addClass("svg");
+
+  // Change the text
+  $('#psychologist p').text('Talk to me. Tell me how you feel by writing your feelings down.');
+
+  // Change the paragraph colour to white
+  $('#psychologist p').css({
+    'color':'white'
+  });
+};
+
+// FUNCTION 2: The state the psychologist section returns to when it is minimized.
+function elizaConceal(){
+
+  // Hide the eliza robot
+  $('#elizarobot').hide();
+
+  // Return the background color to the original blue
+  $('#psychologist').css({
+    'background-color':'#1ca4d6'
+  });
+
+  // Return the paragraph text to its original text
+  $('#psychologist p').text('This service will listen to you and guide you in the right direction.');
+
+  // Return the paragraph colour to dark blue 
+  $('#psychologist p').css({
+    'color':'#10143e'
+  });
+};
