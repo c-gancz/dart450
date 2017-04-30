c6c179d72eaf79334ce78008a37f1f8f1ddc90e9
c-gancz <christina_gancz@hotmail.ca>
2017-04-30 19:43:02 -0400

    Divided the list of drugs alphabetically

--- Assignments/Assignment2/js/pharmacy_community.js
+++ Assignments/Assignment2/js/pharmacy_community.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
 
+  // Call on the drugs.json file with the data of all the pharmaceutical drugs
   $.getJSON('../data/drugs.json', getDrugs);
 
 });
 
+////////////////////////// FUNCTIONS ///////////////////////
+
+// FUNCTION 1: The function that allows the pharmacy to append various parts of the drugs data
 function getDrugs(data){
 
-  var dataDrugs = data.drugsa;
+  // Variable that calls on the drugs starting with the letter "a"
+  var dataDrugs = data.drugsa, i;
 
-  var dataString = dataDrugs.join("");
-
-  $('.drugs').text(dataString);
-
-  console.log(dataString);
-  // $();
-  // testarray.join("")
+  // For loop that allows the the drugs to be displayed one on top of the other instead of inline
+  for( i=0; i< dataDrugs.length; i++ )
+  {
+    $('.drugs').append(dataDrugs[i] + '<br />');
+  }
 
 
 };
